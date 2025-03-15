011f6bd704ba14dd36255066a764b8df58a1750b
umairo790 <umairoabroim6@gmail.com>
2025-03-15 18:10:09 +0500

    fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.WhiteNoiseMiddleware"
 ]
 
 ROOT_URLCONF = "mysite.urls"
